8e6be491df1b8fbea79d4ee26c082cdd86f33601
CodingWithKevinForFin <118689980+CodingWithKevinForFin@users.noreply.github.com>
2025-08-01 10:08:07 +0800

    reapply .gitignore rules

--- amione/src/main/scripts/tools.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-java  -classpath ../lib/out.jar com.f1.ami.amicommon.AmiTools %*
--- amione/src/main/scripts/tools.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`;APPDIR=`pwd`;cd - > /dev/null
-
-cd "$APPDIR/.."
-
-java  -classpath $APPDIR/../lib/out.jar com.f1.ami.amicommon.AmiTools "$@" 
