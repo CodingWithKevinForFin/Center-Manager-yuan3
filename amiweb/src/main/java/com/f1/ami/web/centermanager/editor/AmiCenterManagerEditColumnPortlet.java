acf1e8e708751fb7294f4e2b8857b860b42cd6cb
CodingWithKevinForFin <118689980+CodingWithKevinForFin@users.noreply.github.com>
2025-08-07 15:08:32 +0800

    remove sort on position from table

--- amiweb/src/main/java/com/f1/ami/web/centermanager/editor/AmiCenterManagerEditColumnPortlet.java
+++ amiweb/src/main/java/com/f1/ami/web/centermanager/editor/AmiCenterManagerEditColumnPortlet.java
@@ -160,7 +160,6 @@ public class AmiCenterManagerEditColumnPortlet extends AmiCenterManagerAbstractE
 		editableColumnIds.put("cache", new TableEditableColumn("cache", WebColumnEditConfig.EDIT_CHECKBOX));
 		editableColumnIds.put("cacheValue", new TableEditableColumn("cacheValue", WebColumnEditConfig.EDIT_TEXTFIELD));
 
-		this.columnMetadata.getTable().sortRows("position", true, true, false);
 		this.columnMetadata.setDialogStyle(AmiWebUtils.getService(getManager()).getUserDialogStyleManager());
 		this.columnMetadata.addOption(FastTablePortlet.OPTION_TITLE_BAR_COLOR, "#6f6f6f");
 		this.columnMetadata.addOption(FastTablePortlet.OPTION_TITLE_DIVIDER_HIDDEN, true);
